739ade881de32e6e09ea81a5a4a03ea404ce3535
Kamal Yadav <indianhealper@gmail.com>
2025-07-22 13:03:26 +0530

    video tab is working now

--- main.py
+++ main.py
@@ -1,45 +1,36 @@
 from flask import Flask, jsonify, render_template, request
 import requests
-import math
+import random
 app=Flask("__name__")
-
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "GET":
         return render_template("index.html")
-
-@app.route("/youtube", methods=["GET", "POST"])
-def yt():
-    if request.method == "FETCH":
-        limit_urls_in_each_request = 4
-        urls_json = {}
-        all_urls = requests.get('https://raw.githubusercontent.com/hackesofice/Z/refs/heads/main/Acode-Browser-youtube/Usefull-Videos.txt')
-
-        if all_urls.status_code == 200:
-            urls_list = all_urls.splitLines()
-            i = 0
-            while i <= limit_urls_in_each_request:
-                url = math.random(urls_list)
-                urls_json.update(url)
-            
-            return urls_json
-       
-    elif request.method == "GET":
-        return render_template("yt.html")
     
+@app.route("/youtube/get_video_link", methods=["GET"])
+def fetch_videos():
+    if request.method == "GET":
+        all_urls = requests.get('https://raw.githubusercontent.com/hackesofice/Z/refs/heads/main/Acode-Browser-youtube/Usefull-Videos.txt')
+        if all_urls.status_code == 200:
+            urls_list = all_urls.text.splitlines()
+            random_url = random.choice(urls_list).split("/")[-1]
+            # print(random_url)
+            # url_code = random_url.split("/")[-1]
+            # print(url_code)
+            return jsonify({"uri": random_url}), 200
+            
+    else:
+        return jsonify({"message":"method isnt allowed"}), 405
+
+
+
+
+@app.route("/youtube", methods=["GET"])
+def yt():   
+    if request.method == "GET":
+        return render_template("yt.html")
     else:
         print("! invalid request")
-        return jsonify({"message":"!invalid request"}), 401
-
-
-
-
-
-
-
-
-
-
-
-
+        return jsonify({"message":"! method is not allowed"}), 405
+    
 app.run()
--- static/JS/yt.js
+++ static/JS/yt.js
@@ -4,7 +4,6 @@ let loader = document.getElementById("loader");
     let observer = new IntersectionObserver((element) => {
         if (element[0].isIntersecting){
            insert_new_videos(5);
-            console.log(" showing the lpafder ")
         }
     });
 
@@ -13,13 +12,44 @@ observer.observe(loader);
 // for now im using it without any agrument in future i'll use an list of youtube urls 
 function insert_new_videos(number_of_wanted_videos){
         for (let i = 0; i < number_of_wanted_videos; i++){
-            setTimeout(()=>{
-                console.log("loading ");
+            ask_for_url_from_server()
+            .then(response =>{
+                setTimeout(()=> {
+                console.log(` calld from insert video function response is ==> ${response.uri}`);
                 let new_box = document.createElement("div");
                     new_box.className = "video";
                     main_container.appendChild(new_box);
-            }, 3000);
+                    let iframe = document.createElement("iframe");
+                        iframe.src =`https://www.youtube.com/embed/${response.uri}`;
+                        iframe.title = "Acode - Video Player";
+                        iframe.style.cssText = `width: 100%; height: 100%;`;
+                        new_box.appendChild(iframe)
+            }, 1000)});
+            // setTimeout(()=>{
+                
+
+            // }, 1000);
 
             
         }
 }
+
+async function ask_for_url_from_server(){
+    try {
+        const response =  await fetch("/youtube/get_video_link", {
+            method: "GET"
+        });
+        return response.json()
+    }catch(err){
+        console.log(`hey dear we got some err ==>>> ${err}`);
+    }
+}
+
+
+
+
+
+
+
+
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/hLAc7OO3wHI?si=6-94vxnnotvWzS-F" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
